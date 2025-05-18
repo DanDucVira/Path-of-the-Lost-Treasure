82ae2e2178c4bbe49a07a27a9ad8b842609a592f
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2025-05-18 16:35:45 -0400

    added starter files

---
+++ game.py
@@ -0,0 +1,15 @@
+print('''              
+###############################
+#                             #
+#  PATH OF THE LOST TREASURE  #
+#                             #
+###############################
+
+''')
+
+print('''
+You stand at the edge of the Isle of Whispers. A mystical fog swirls around you, and a narrow path splits before you.
+      
+Legend tells of the Heart of Eloria — a gem of unimaginable power — buried deep within the jungle.
+      
+A voice echoes in your mind: “Only one path leads to your destiny. Choose wisely...”''')
