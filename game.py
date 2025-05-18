6084e410ea57f522008d1a71c66269d8e8d73948
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2025-05-18 17:51:14 -0400

    restart file and added starter files

--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+
+
 print('''              
 ###############################
 #                             #
@@ -6,10 +8,3 @@ print('''
 ###############################
 
 ''')
-
-print('''
-You stand at the edge of the Isle of Whispers. A mystical fog swirls around you, and a narrow path splits before you.
-      
-Legend tells of the Heart of Eloria — a gem of unimaginable power — buried deep within the jungle.
-      
-A voice echoes in your mind: “Only one path leads to your destiny. Choose wisely...”''')
